42ebd2093a3747c12c1d455b1f1c0bc3808b0003
jason.williams <jason.williams@paylinksolutions.co.uk>
2020-12-31 17:19:23 +0000

    stage-three form array example

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -4,6 +4,8 @@ import {
     FormGroup,
     FormControl,
     Validators,
+    FormBuilder,
+    FormArray,
 } from '@angular/forms';
 
 @Component({
@@ -24,9 +26,18 @@ export class ProfileEditorComponent {
             state: new FormControl(''),
             zip: new FormControl(''),
         }),
+        aliases: this.fb.array([this.fb.control('')]),
     });
 
     onSubmit() {
         console.log(this.profileForm.value);
+    }
+
+    get aliases() {
+        return this.profileForm.get('aliases') as FormArray;
+    }
+
+    addAlias() {
+        this.aliases.push(this.fb.control(''));
     }
 }
